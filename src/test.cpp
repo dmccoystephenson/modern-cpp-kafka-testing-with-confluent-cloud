ac5ad673c6d715a49ae9458a350755104855836a
Daniel Stephenson <dstephenson@trihydrol.com>
2022-02-09 09:19:16 -0700

    Implemented some code to send a message with a producer.

--- src/test.cpp
+++ src/test.cpp
@@ -1,3 +1,5 @@
+// using this as a reference https://github.com/morganstanley/modern-cpp-kafka/blob/main/examples/kafka_sync_producer.cc
+
 #include <string>
 #include <iostream>
 #include "kafka/KafkaProducer.h"
@@ -19,7 +21,36 @@ const char* getEnvironmentVariable(const char* variableName) {
 int main() {
     log("Executing program.");
 
-    // TODO: implement
+    string brokers = "";
+    string topic = "";
+    string message = "";
+
+    try {
+        // create config object
+        kafka::Properties properties({
+            {"bootstrap.servers", brokers}
+            {"enable.idempotence", "true"}
+        })
+
+        // create producer
+        KafkaProducer producers(properties):
+
+        // prepare record
+        auto record = producer::ProducerRecord(topic, kafka::NullKey, Kafka::Value(message.c_str(), message.size());
+
+        // send message
+        try {
+            producer::RecordMetaData metadata = producer.syncSend(record);
+            log("Message delivered.");
+
+        } catch(const kafka::KafkaException& e) {
+            cout << "[ERROR] Message delivery failed: " << e.error().message() << endl;
+        }
+
+
+    } catch(kafka::KafkaException& e) {
+        cout << "[ERROR] Unexpected exception: " << e.what() << endl;
+    }
 
     log("Finished executing program.");
     return 0;
